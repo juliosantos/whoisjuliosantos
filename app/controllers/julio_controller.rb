063d609067a1f732f436b1b46010801a03707045
Júlio Santos <julio.ademar@gmail.com>
2011-06-18 15:14:34 +0200

    v0.1

---
+++ app/controllers/julio_controller.rb
@@ -0,0 +1,10 @@
+class JulioController < ApplicationController
+  def index
+  end
+
+  def cv
+  end
+
+  def contact
+  end
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var Julio = function () {
+  $( document ).ready( function () {
+    var bodyID = $( "body" ).attr( "id" );
+
+    if ( bodyID.match( /julio-index/ ) ) {
+    }
+  });
+
+}();
+
+
