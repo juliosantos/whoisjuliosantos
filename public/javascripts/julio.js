30494a570df6cd22c63227579874863128d3fcae
Júlio Santos <julio.ademar@gmail.com>
2011-06-19 17:38:21 +0200

    ...

--- public/javascripts/julio.js
+++ public/javascripts/julio.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-	$("body").ezBgResize({
+$( document ).ready( function () {
+	$( "body" ).ezBgResize({
 		img : "/images/background.jpg"
 	});
+
+  setTimeout( function(){ floatbg(-1) }, 1000 );
 });
+
+function floatbg (direction) {
+  var background = $( "div#jq_ez_bg" ).children( "img" );
+  old_top = parseInt( background.css( "top" ).replace( "px", "" ) );
+
+  background.animate({
+    "top" : old_top - direction * 60
+  }, 6000, "easeInOutQuad", function () {
+    floatbg( -direction );
+  });
+};
