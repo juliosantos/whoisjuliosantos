239d5a016a8789fd76dbeee7129792b8a9a8ce09
Júlio Santos <julio.ademar@gmail.com>
2011-06-21 18:40:18 +0200

    hypnotoad

--- public/javascripts/julio.js
+++ public/javascripts/julio.js
@@ -1,6 +1,13 @@
 $( document ).ready( function () {
-  $.backstretch( "/images/background.jpg", { speed: 500 }, zoombg );
+  $.backstretch( "/images/background.jpg", { speed: 500 }, function () { zoombg( 1 ); } );
+  $( window ).konami( hypnoToad );
 });
+
+function hypnoToad () {
+  console.log("BANG");
+  $( "div#backstretch" ).remove();
+  $( "body" ).css({ "background" : "#FFF url( /images/hypnotoad.gif )" });
+};
 
 function floatbg (direction) {
   var background = $( "div#jq_ez_bg" ).children( "img" );
@@ -13,16 +20,24 @@ function floatbg (direction) {
   });
 };
 
-function zoombg () {
+function zoombg (direction) {
   var background = $( "div#backstretch img" );
   old_top = parseInt( background.css( "top" ).replace( "px", "" ) );
   old_left = parseInt( background.css( "left" ).replace( "px", "" ) );
 
+  new_width = background.width() * Math.pow( 2, direction );
+  new_height = background.height() * Math.pow( 2, direction );
+
+  // thanks @hugopeixoto for helping me get rid of the ifs :D
+  new_top = old_top - direction * background.height() / ( 3 - direction );
+  new_left = old_left - direction * background.width() / ( 3 - direction );
+
   background.animate({
-    "width" : background.width() * 2,
-    "height" : background.height() * 2,
-    "top" : old_top - background.height() / 2,
-    "left" : old_left - background.width() / 2
-  }, 40000, "linear", function () {
+    "width" : new_width,
+    "height" : new_height,
+    "top" : new_top,// - background.height() / 2,
+    "left" : new_left// - background.width() / 2
+  }, 20000, "easeInOutQuad", function () {
+    zoombg( -direction );
   });
 };
